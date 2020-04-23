0efc0c74ef93a9399e477ba95cb6795b8f9d1d86
Lauren Thorud <52000191+LalaIAm@users.noreply.github.com>
2020-04-23 06:03:15 -0500

    Updates

---
+++ precache-manifest.beacc31d4572f1fc03d84afe41545153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5ec0451e5e4236fad6c0587693b9f2",
+    "url": "/would-you-rather/index.html"
+  },
+  {
+    "revision": "aab0b09543c7d45144a0",
+    "url": "/would-you-rather/static/css/main.c67750ac.chunk.css"
+  },
+  {
+    "revision": "e12e9e197a3b99fc0dae",
+    "url": "/would-you-rather/static/js/2.e5b1ff63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/would-you-rather/static/js/2.e5b1ff63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab0b09543c7d45144a0",
+    "url": "/would-you-rather/static/js/main.27e38791.chunk.js"
+  },
+  {
+    "revision": "bc5fb8d44b1da14bd6db",
+    "url": "/would-you-rather/static/js/runtime-main.52353399.js"
+  },
+  {
+    "revision": "966818d082840d2952daa8b162fa7a20",
+    "url": "/would-you-rather/static/media/user5.966818d0.png"
+  }
+]);
